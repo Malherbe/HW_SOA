dddf7152124a8ea6ff09e7b42c85a0ae4cb31155
Malherbe <malherbe.billy@gmail.com>
2021-10-11 15:38:12 +0800

    All tests pass; rubocop/flog resolved

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,55 @@
+# frozen_string_literal: true
+
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
-  def take_tsv(tsv)
 
+  require 'yaml'
+  attr_accessor :data
+
+  TAB = "\t"
+  NEWLINE = "\n"
+
+  def take_tsv(tsv)
+    @data = []
+    lines = tsv.split(NEWLINE).map { |line| line.split(TAB) }
+    keys = lines.shift
+    lines.each do |line|
+      hash = {}
+      line.each_index do |value|
+        hash[keys[value]] = line[value].chomp
+      end
+      @data << hash
+    end
   end
 
-  # to_tsv: converts @data into tsv string
-  # returns: String in TSV format
+  #   # to_tsv: converts @data into tsv string
+  #   # returns: String in TSV format
   def to_tsv
+    yaml_file = @data
+    keys = yaml_file[0].keys
+    total = ''
+    keys.each_with_index do |_val, index|
+      data = index == keys.length - 1 ? keys[index] : keys[index] + TAB
+      total += data
+    end
+    tsv = total
+    tsv.concat(NEWLINE)
+    yaml_file.each do |hash|
+      value = get_files(hash)
+      tsv.concat(value)
+      tsv.concat(NEWLINE)
+    end
+    tsv
+  end
 
+  def get_files(hash)
+    result = ''
+    hash.each_with_index do |(_key, value), index|
+      data = index == hash.length - 1 ? value : "#{value}#{TAB}"
+      result += data
+    end
+    result
   end
 end
